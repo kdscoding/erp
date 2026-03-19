b8673d40b5cdb652754babd8ce9eb728d99b1a8f
project_kdscoding <kds.coding@gmail.com>
2026-03-19 14:34:23 +0700

    Resolve conflict

--- erp-monitoring-po/resources/views/layouts/erp.blade.php
+++ erp-monitoring-po/resources/views/layouts/erp.blade.php
@@ -109,11 +109,12 @@
         </section>
     </div>
 
-    <footer class="main-footer"><strong>Procurement Control</strong> - Light UI Operational Dashboard</footer>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
-</body>
+    <footer class="main-footer">
+    <strong>Procurement Control</strong> - Light UI Operational Dashboard</footer>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+    </body>
 
 </html>
